66ae39e8715f3afb2d34ddf53ebdb110678195db
Caro <carito19@gmail.com>
2022-11-09 00:54:53 -0400

    Project

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+export default function FormDialog() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Abrir modal
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Envia un Mensaje</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Escribe el mensaje
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Salir</Button>
+          <Button onClick={handleClose}>Enviar</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
